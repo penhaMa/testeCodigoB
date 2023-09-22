07fe7d5e9ab9d7e079ed4682e3bfc88a50938a3c
penhaMa <130068243+penhaMa@users.noreply.github.com>
2023-09-22 12:37:21 -0300

    Primeiro commit

---
+++ testeCodigodeBarras/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace testeCodigodeBarras
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.camera = new System.Windows.Forms.ComboBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.textCBarra = new System.Windows.Forms.TextBox();
+            this.start = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Camera:";
+            // 
+            // camera
+            // 
+            this.camera.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.camera.FormattingEnabled = true;
+            this.camera.Location = new System.Drawing.Point(79, 28);
+            this.camera.Name = "camera";
+            this.camera.Size = new System.Drawing.Size(438, 24);
+            this.camera.TabIndex = 1;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox.Location = new System.Drawing.Point(12, 58);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(707, 288);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // textCBarra
+            // 
+            this.textCBarra.Location = new System.Drawing.Point(144, 352);
+            this.textCBarra.Name = "textCBarra";
+            this.textCBarra.Size = new System.Drawing.Size(575, 22);
+            this.textCBarra.TabIndex = 3;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(617, 377);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(102, 32);
+            this.start.TabIndex = 4;
+            this.start.Text = "Começar";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 355);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Codígo de Barra:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 421);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.textCBarra);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.camera);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Codígo de Barras";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox camera;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox textCBarra;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ testeCodigodeBarras/Form1.cs
@@ -0,0 +1,63 @@
+﻿using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace testeCodigodeBarras
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach(FilterInfo device in filterInfoCollection)
+                camera.Items.Add(device.Name);
+            camera.SelectedIndex = 0;
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[camera.SelectedIndex].MonikerString);
+            videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            videoCaptureDevice.Start();
+        }
+
+        private void VideoCaptureDevice_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            BarcodeReader reader = new BarcodeReader();
+            var result = reader.Decode(bitmap);
+            if (result != null) 
+            {
+                textCBarra.Invoke(new MethodInvoker(delegate ()
+                {
+                    textCBarra.Text = result.ToString();
+                }));
+            }
+            pictureBox.Image = bitmap;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoCaptureDevice != null)
+            { 
+                if(videoCaptureDevice.IsRunning)
+                    videoCaptureDevice.Stop();
+            }
+        }
+    }
+}
